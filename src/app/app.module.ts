952a169c15f09db0b44bc765d4f74750398ac277
Andy <andycruz08.02@gmail.com>
2025-07-02 20:49:43 +0200

    created pwa succesfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { LeftBoardComponent } from './components/left-board/left-board.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent, LeftBoardComponent],
-  imports: [BrowserModule, DragDropModule],
+  imports: [BrowserModule, DragDropModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [],
   bootstrap: [AppComponent],
 })
